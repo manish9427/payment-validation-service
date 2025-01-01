632fe5f2f923e5875a42d6a76f9a144473677cb6
manish9427 <manish119427@gmail.com>
2025-01-01 23:49:52 +0530

    1 jab

--- src/main/java/com/cpt/payments/service/impl/PaymentServiceImpl.java
+++ src/main/java/com/cpt/payments/service/impl/PaymentServiceImpl.java
@@ -19,44 +19,44 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class PaymentServiceImpl implements PaymentService {
 
-	
+
 	private ApplicationContext applicationContext;
-	
+
 	private ValidationRuleDAO validationRuleDAO;
-	
+
 	private List<String> activeValidatorRules;
-	
+
 	public PaymentServiceImpl(ApplicationContext applicationContext,ValidationRuleDAO validationRuleDAO) {
 		this.applicationContext = applicationContext;
 		this.validationRuleDAO = validationRuleDAO;
 	}
-	
+
 	@Override
 	public PaymentResponseDTO validateAndProcessPayment(PaymentRequestDTO paymentRequest) {
-		
+
 		//String name = null;
 		//name.length();
-		
+
 		log.info("Payment request received: {}", paymentRequest);
 		log.info("Active validator rules:{}", activeValidatorRules);
-		
+
 		activeValidatorRules.forEach(rule -> triggerValidationRule(paymentRequest, rule));
-		
+
 
 		// return errorCode & errorMessage
-		
-		
+
+
 		log.info("Payment request processed successfully. All rules passed");
-		
+
 		//TODO invoke processing service for further processing paymentRequest
 		// expect some txnId & redirectUrl from processing service
 		String txnId = "TX123";
 		String redirectUrl = "http://www.google.com";
-		
-		
+
+
 		PaymentResponseDTO paymentResponse = new PaymentResponseDTO(
 				txnId, redirectUrl);
-		
+
 		log.info("Payment response: {}", paymentResponse);
 		return paymentResponse;
 	}
@@ -64,34 +64,34 @@ public class PaymentServiceImpl implements PaymentService {
 	private String triggerValidationRule(PaymentRequestDTO paymentRequest, String rule) {
 		rule = rule.trim();
 		log.info("Validating payment request using rule: {}", rule);
-		
+
 		Validator validator = null;
-		
+
 		Class<? extends Validator> validatorClass = ValidatorEnum.getClassByName(rule);
-		
+
 		if (validatorClass != null) {
 			validator = applicationContext.getBean(validatorClass);
-			
+
 			if (validator != null) {
 				log.info("Calling validator rule rule: {}", rule);
 				validator.validate(paymentRequest);
 			}
 		}
-		
+
 		if (validatorClass == null || validator == null) {
 			log.error("Either Validator class not found or Validator instance not found for "
 					+ "|rule:{} | validatorClass:{} | validator:{}", 
 					rule, validatorClass, validator);
 			//continue;
 		}
-		
+
 		// RUle ran successfully
 		return rule;
 	}
 	@PostConstruct
 	public void loadValidatorRule() {
 		activeValidatorRules = validationRuleDAO.loadActiveValidatorNames();
-        log.info("Loaded active validators rules from DB: {}", activeValidatorRules);
+		log.info("Loaded active validators rules from DB: {}", activeValidatorRules);
 
 	}
 
